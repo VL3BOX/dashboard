715898b7139af4085df9b89d22e09a442fc839ed
浮烟 <rx6@qq.com>
2020-04-21 18:34:07 +0800

    Initial commit

---
+++ src/main_blank.js
@@ -0,0 +1,18 @@
+Vue.config.productionTip = false;
+
+// import Vue from "vue";
+// import ElementUI from "element-ui";
+// import "element-ui/lib/theme-chalk/index.css";
+// Vue.use(ElementUI);
+
+import JX3BOX_UI from '@jx3box/jx3box-common/vue'
+Vue.use(JX3BOX_UI);
+
+// import router from "../router";
+// import store from "../store";
+
+import App from "./App.vue";
+new Vue({
+    render: h => h(App),
+}).$mount("#app");
+

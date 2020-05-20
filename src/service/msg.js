b2cfa816f16f9c09d815e17af895e5a872d250eb
Kuguats <445786355@qq.com>
2020-05-20 23:52:22 +0800

    消息删除/已读

--- src/service/msg.js
+++ src/service/msg.js
@@ -1,18 +1,32 @@
-import { axios as $ } from "./axios";
+import {$} from "./axios";
 import {__helperUrl} from '@jx3box/jx3box-common/js/jx3box.json'
-const API = __helperUrl + 'api/messages'
+const API = __helperUrl + 'api/messages';
+const qs = require('qs');
 
-function getMsgs(i=1) {
-    return $.get(API,{
-        headers : {
-            Accept : 'application/prs.helper.v2+json'
-        },
-        withCredentials: true,
-        params : {
+function getMsgs(i = 1) {
+    return $.get(API, {
+        headers: {Accept: 'application/prs.helper.v2+json'},
+        params: {
             // length : 2,
-            page :i
+            page: i
         }
     })
-
 }
-export { getMsgs }
+
+function readMsg(ids) {
+    return $.put(`${API}/read`, qs.stringify({
+        ids: ids
+    }), {
+        headers: {Accept: 'application/prs.helper.v2+json'},
+    })
+}
+
+function removeMsg(ids) {
+    return $.put(`${API}/remove`, qs.stringify({
+        ids: ids
+    }), {
+        headers: {Accept: 'application/prs.helper.v2+json'},
+    })
+}
+
+export {getMsgs, readMsg, removeMsg}
--- src/views/msg.vue
+++ src/views/msg.vue
@@ -5,7 +5,7 @@
                 <h4 class="u-title">消息列表</h4>
             </div>
             <ul class="m-dashboard-box-list" v-if="data.length">
-                <li v-for="(item, i) in data" :key="i" :class="{on : item.read == 1}">
+                <li v-for="(item, i) in data" :key="i" :class="{on : item.read == 1}" v-show="item.deleted==0">
                     <span class="u-content">
                         {{ item.content }}
                     </span>
@@ -18,17 +18,16 @@
                             size="mini"
                             icon="el-icon-check"
                             title="设为已读"
-                            @click="read(item.id)"
+                            @click="read(item)"
                             :disabled="item.read == 1"
                         ></el-button>
-                        <!-- TODO:添加删除消息 -->
-                        <!-- <el-button
+                        <el-button
                             type="primary"
                             size="mini"
                             icon="el-icon-delete"
                             title="删除"
-                            @click="del(item.id)"
-                        ></el-button> -->
+                            @click="del(item)"
+                        ></el-button>
                     </el-button-group>
                 </li>
             </ul>
@@ -57,7 +56,7 @@
 </template>
 
 <script>
-import { getMsgs } from "../service/msg.js";
+import { getMsgs, readMsg, removeMsg } from "../service/msg.js";
 import dateFormat from '../utils/dateFormat'
 export default {
     name: "msg",
@@ -75,7 +74,24 @@ export default {
             getMsgs(i).then((res) => {
                 this.total = res.data.data.total;
                 this.data = res.data.data.messages;
-                console.log(res.data.data);
+            });
+        },
+        read(item){
+            readMsg([item.ID]).then((res) => {
+                if(res.data.code === 200) {
+                    item.read = 1;
+                } else {
+                    this.$notify.error({title: res.data.message});
+                }
+            });
+        },
+        del(item){
+            removeMsg([item.ID]).then((res) => {
+                if(res.data.code === 200) {
+                    item.deleted = Math.round(new Date() / 1000);
+                } else {
+                    this.$notify.error({title: res.data.message});
+                }
             });
         },
         dateFormat : function (val){

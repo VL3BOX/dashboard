238ddee0c768843e2aa8a117a22ebd5b366da62b
iRuxu <rx6@qq.com>
2020-08-22 20:33:10 +0800

    update

--- src/service/share.js
+++ src/service/share.js
@@ -1,15 +1,14 @@
 import { $ } from "./axios";
-import { __node, __Root } from "@jx3box/jx3box-common/js/jx3box.json";
+import { __node, __Root, __server } from "@jx3box/jx3box-common/js/jx3box.json";
 
 function uploadData(formdata, vm) {
-    return $.post(__server + "upload/data", formdata)
+    return $.post(__server + "upload/data", formdata);
 }
 
-function parseData(data,vm) {
+function parseData(data, vm) {
     return $.post(__node + "facedat", {
-        raw : data
-    })
-    
+        raw: data,
+    });
 }
 
 export { uploadData, parseData };

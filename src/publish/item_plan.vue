100457bec6af41f0758296e85cb02c4c0931e811
Kuguats <445786355@qq.com>
2020-11-08 19:24:42 +0800

    fix

--- src/publish/item_plan.vue
+++ src/publish/item_plan.vue
@@ -22,16 +22,20 @@
       </el-col>
 
       <el-col :xs="24" :sm="19" class="c-plan-relation">
-        <div class="m-position" v-for="(position,key) in positions" :key="key">
-          <h3 class="u-title" v-text="position.label"></h3>
-          <draggable class="m-selected-items" tag="ul" :list="selecteds" group="people" :move="move_handle"
-                     :data-AucGenre="position.AucGenre" :data-AucSubType="position.AucSubType"
-                     style="min-height:60px;border: 1px solid;border-radius: 4px;">
-            <li v-for="(item,key) in selecteds" :key="key">
-              <Item :item="item"/>
-            </li>
-          </draggable>
-        </div>
+        <el-row :gutter="15">
+          <el-col :xs="24" :sm="6" class="m-position" v-for="(position,key) in positions" :key="key">
+            <h3 class="u-title" v-text="position.label"></h3>
+            <div class="c-selected-items">
+              <draggable class="m-selected-items" tag="ul" :list="plan.relation[key]" group="people" :move="move_handle"
+                         :data-AucGenre="position.AucGenre" :data-AucSubType="position.AucSubType">
+                <li v-for="(item,key) in plan.relation[key]" :key="key">
+                  <Item :item="item"/>
+                </li>
+              </draggable>
+              <span v-if="!plan.relation[key].length" class="u-tip">拖拽所需装备到此处</span>
+            </div>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
   </div>
@@ -57,58 +61,70 @@ export default {
     return {
       keyword: '',
       items: null,
-      selecteds: [],
       plan: {
         id: '',
         name: '',
         type: '',
-        relation: '',
+        relation: {
+          '1': [],
+          '2': [],
+          '3_1': [],
+          '3_2': [],
+          '3_3': [],
+          '3_4': [],
+          '3_5': [],
+          '3_6': [],
+          '4_1': [],
+          '4_3': [],
+          '4_2_1': [],
+          '4_2_2': [],
+        },
         description: '',
       },
-      positions: [
-        {label: '武器', AucGenre: 1},
-        {label: '暗器', AucGenre: 2},
-        {label: '上衣', AucGenre: 3, AucSubType: 1},
-        {label: '帽子', AucGenre: 3, AucSubType: 2},
-        {label: '腰带', AucGenre: 3, AucSubType: 3},
-        {label: '下装', AucGenre: 3, AucSubType: 4},
-        {label: '鞋子', AucGenre: 3, AucSubType: 5},
-        {label: '护腕', AucGenre: 3, AucSubType: 6},
-        {label: '项链', AucGenre: 4, AucSubType: 1},
-        {label: '腰坠', AucGenre: 4, AucSubType: 3},
-        {label: '戒指', AucGenre: 4, AucSubType: 2},
-        {label: '戒指', AucGenre: 4, AucSubType: 2},
-      ],
+      positions: {
+        '1': {label: '武器', AucGenre: 1},
+        '2': {label: '暗器', AucGenre: 2},
+        '3_1': {label: '上衣', AucGenre: 3, AucSubType: 1},
+        '3_2': {label: '帽子', AucGenre: 3, AucSubType: 2},
+        '3_3': {label: '腰带', AucGenre: 3, AucSubType: 3},
+        '3_4': {label: '下装', AucGenre: 3, AucSubType: 4},
+        '3_5': {label: '鞋子', AucGenre: 3, AucSubType: 5},
+        '3_6': {label: '护腕', AucGenre: 3, AucSubType: 6},
+        '4_1': {label: '项链', AucGenre: 4, AucSubType: 1},
+        '4_3': {label: '腰坠', AucGenre: 4, AucSubType: 3},
+        '4_2_1': {label: '戒指', AucGenre: 4, AucSubType: 2, index: 1},
+        '4_2_2': {label: '戒指', AucGenre: 4, AucSubType: 2, index: 2},
+      },
     };
   },
   computed: {},
   methods: {
-    move_handle(e, to) {
-      if (to.target.classList.contains('m-selected-items')) {
-        let AucGenre = to.target.getAttribute('data-AucGenre');
-        let AucSubType = to.target.getAttribute('data-AucSubType');
-        console.log(44,e.draggedContext.element);
-        let result = e.draggedContext.element.AucGenre == AucGenre;
-        if (AucSubType !== null) result = result && e.draggedContext.element.AucSubType == AucSubType;
-        return result;
-      }
+    move_handle(e) {
+      if (!e.to.classList.contains('m-selected-items')) return false;
+      let AucGenre = e.to.getAttribute('data-AucGenre');
+      let AucSubType = e.to.getAttribute('data-AucSubType');
+      let result = e.draggedContext.element.AucGenre == AucGenre;
+      if (AucSubType !== null) result = result && e.draggedContext.element.AucSubType == AucSubType;
+      return result;
     },
   },
   mounted() {
 
   },
   watch: {
-    keyword() {
-      search_items(this.keyword, 10, ['id', 'UiID', 'Name', 'IconID', 'Quality', 'AucGenre', 'AucSubType']).then(
-          (data) => {
-            data = data.data;
-            this.items = data.code === 200 ? data.data.data : [];
-            console.log(41412,this.items);
-          },
-          () => {
-            this.options.items = false;
-          }
-      );
+    keyword: {
+      immediate: true,
+      handler(){
+        search_items(this.keyword, 15, ['id', 'UiID', 'Name', 'IconID', 'Quality', 'AucGenre', 'AucSubType']).then(
+            (data) => {
+              data = data.data;
+              this.items = data.code === 200 ? data.data.data : [];
+            },
+            () => {
+              this.options.items = false;
+            }
+        );
+      }
     }
   },
   components: {

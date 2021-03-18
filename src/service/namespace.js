e4a2d43710d1177d10fd2677822e1a20e5b5d664
iRuxu <rx6@qq.com>
2021-03-18 16:36:52 +0800

    fix proxy

--- src/service/namespace.js
+++ src/service/namespace.js
@@ -1,33 +1,39 @@
-import { $_helper, $helper, axios } from './axios'
-import { $cms } from '@jx3box/jx3box-common/js/https'
+import { $_helper, $helper, axios } from "./axios";
+import { $cms } from "@jx3box/jx3box-common/js/https";
 
 function getNamespace() {
-  return $_helper({
-    method: 'GET',
-    url: `/api/my/namespaces`,
-  })
+    return $_helper({
+        method: "GET",
+        url: `/api/my/namespaces`,
+    });
 }
 
 function getNamespaceByKey(key) {
-  return $helper({
-    method: 'GET',
-    url: `/api/namespace`,
-    params: {
-      key,
-    },
-  })
+    return $helper({
+        method: "GET",
+        url: `/api/namespace`,
+        params: {
+            key,
+        },
+    });
 }
 
 function createNamespace(data) {
-  return $cms().post('/api/cms/namespace', data)
+    return $cms().post("/api/cms/namespace", data);
 }
 
 function updateNamespace(id, data) {
-  return $cms().put(`/api/cms/namespace/${id}`, data)
+    return $cms().put(`/api/cms/namespace/${id}`, data);
 }
 
 function queryNamespace(id) {
-  return $cms().get(`/api/cms/namespace/${id}`)
+    return $cms().get(`/api/cms/namespace/${id}`);
 }
 
-export { getNamespace, getNamespaceByKey, queryNamespace, createNamespace, updateNamespace }
+export {
+    getNamespace,
+    getNamespaceByKey,
+    queryNamespace,
+    createNamespace,
+    updateNamespace,
+};

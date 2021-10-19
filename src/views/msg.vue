8d49b861db8c917b537518b0be71b055156eee78
iRuxu <rx6@qq.com>
2021-10-19 20:37:33 +0800

    fix: birthday link

--- src/views/msg.vue
+++ src/views/msg.vue
@@ -22,11 +22,7 @@
             @keyup.enter.native="changePage(1)"
         >
             <template slot="prepend">关键词</template>
-            <el-button
-                slot="append"
-                icon="el-icon-search"
-                @click="changePage(1)"
-            ></el-button>
+            <el-button slot="append" icon="el-icon-search" @click="changePage(1)"></el-button>
         </el-input>
         <ul class="m-dashboard-box-list" v-if="data.length">
             <li
@@ -37,9 +33,7 @@
             >
                 <div class="u-primary">
                     <span class="u-content">
-                        <span class="u-label u-hasChecked" v-if="item.read == 1"
-                            >已读</span
-                        >
+                        <span class="u-label u-hasChecked" v-if="item.read == 1">已读</span>
                         <span class="u-label u-hasNotChecked" v-else>未读</span>
                         <span v-html="item.content"></span>
                         <a
@@ -48,13 +42,14 @@
                             v-if="hasLink(item)"
                             @click="read(item)"
                             target="_blank"
-                            ><i class="el-icon-link"></i> 点击查看</a
                         >
+                            <i class="el-icon-link"></i> 点击查看
+                        </a>
                     </span>
-                    <time class="u-time"
-                        ><i class="el-icon-phone-outline"></i>
-                        {{ dateFormat(~~item.created) }}</time
-                    >
+                    <time class="u-time">
+                        <i class="el-icon-phone-outline"></i>
+                        {{ dateFormat(~~item.created) }}
+                    </time>
                 </div>
                 <el-button-group class="u-action">
                     <el-button
@@ -64,24 +59,11 @@
                         @click="read(item)"
                         :disabled="item.read == 1"
                     ></el-button>
-                    <el-button
-                        size="mini"
-                        icon="el-icon-delete"
-                        title="删除"
-                        @click="del(item)"
-                    ></el-button>
+                    <el-button size="mini" icon="el-icon-delete" title="删除" @click="del(item)"></el-button>
                 </el-button-group>
             </li>
         </ul>
-        <el-alert
-            v-else
-            class="m-dashboard-box-null"
-            title="没有找到相关条目"
-            type="info"
-            center
-            show-icon
-        >
-        </el-alert>
+        <el-alert v-else class="m-dashboard-box-null" title="没有找到相关条目" type="info" center show-icon></el-alert>
         <el-pagination
             class="m-dashboard-box-pages"
             background
@@ -90,8 +72,7 @@
             :current-page="page"
             layout="total, prev, pager, next, jumper"
             :total="total"
-        >
-        </el-pagination>
+        ></el-pagination>
     </div>
 </template>
 
@@ -99,14 +80,14 @@
 import { getMsgs, readMsg, removeMsg } from "../service/msg.js";
 import { showDate, showTime } from "@jx3box/jx3box-common/js/moment.js";
 import { getLink } from "@jx3box/jx3box-common/js/utils";
-import {Base64 } from 'js-base64';
-const ignoreLinkTypes = ['namespace']
+import { Base64 } from "js-base64";
+const ignoreLinkTypes = ["namespace"];
 export default {
     name: "msg",
     props: [],
-    data: function() {
+    data: function () {
         return {
-            keyword: '',
+            keyword: "",
             type: "all",
             data: [],
             unread_total: 0,
@@ -115,11 +96,11 @@ export default {
         };
     },
     methods: {
-        changePage: function(i = 1) {
+        changePage: function (i = 1) {
             this.page = i;
             getMsgs({
                 where: {
-                    content: this.keyword
+                    content: this.keyword,
                 },
                 page: i,
             }).then((res) => {
@@ -150,35 +131,40 @@ export default {
                 }
             });
         },
-        dateFormat: function(val) {
+        dateFormat: function (val) {
             return showTime(new Date(val * 1000));
         },
-        hasLink : function (item){
-            if(ignoreLinkTypes.includes(item.source_type)){
-                return false
-            }else if(item.source_id && item.source_type){
-                return true
+        hasLink: function (item) {
+            if (ignoreLinkTypes.includes(item.source_type)) {
+                return false;
+            } else if (item.source_id && item.source_type) {
+                return true;
             }
-            return false
-        }
+            return false;
+        },
     },
     filters: {
-        msgLink: function(item) {
+        msgLink: function (item) {
             let source_id = item.source_id;
             let source_type = item.source_type;
-            if(source_type == 'birthday'){
-                return `/author/${item.user_id}/#/birthday?code=` + Base64.encode(source_id)
-            }else if(source_type== 'sign'){
-                return `/dashboard/#/cooperation`
-            }else if(source_type == 'callback'){
-                let info = encodeURIComponent(Base64.encode(JSON.stringify(item)))
-                return `/dashboard/#/${item.type}/${item.subtype}?info=${info}`
-            }else{
+            if (source_type == "birthday") {
+                return (
+                    `/author/birthday/${item.user_id}?code=` +
+                    Base64.encode(source_id)
+                );
+            } else if (source_type == "sign") {
+                return `/dashboard/#/cooperation`;
+            } else if (source_type == "callback") {
+                let info = encodeURIComponent(
+                    Base64.encode(JSON.stringify(item))
+                );
+                return `/dashboard/#/${item.type}/${item.subtype}?info=${info}`;
+            } else {
                 return getLink(source_type, source_id);
             }
         },
     },
-    mounted: function() {
+    mounted: function () {
         this.changePage();
     },
 };

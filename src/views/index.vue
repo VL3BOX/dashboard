d822aeb8dd41f458b2e2ff72eead21a98cfe0ddf
iRuxu <rx6@qq.com>
2022-05-15 10:29:18 +0800

    fix:邮箱验证

--- src/views/index.vue
+++ src/views/index.vue
@@ -47,18 +47,18 @@
                         <el-tooltip
                             class="item"
                             effect="dark"
-                            :content="~~info.verify_email ? '✔️邮箱已验证' : '邮箱未验证'"
+                            :content="~~info.verify_email ? '邮箱已验证' : '邮箱未验证'"
                             placement="top"
                         >
-                            <a href="/dashboard/#/email"><i class="el-icon-message"></i></a>
+                            <a href="/dashboard/#/email" :class="info.verify_email && 'done'"><i class="el-icon-message"></i></a>
                         </el-tooltip>
                         <el-tooltip
                             class="item"
                             effect="dark"
-                            :content="~~info.verify_phone ? '✔️手机已绑定' : '手机未绑定'"
+                            :content="~~info.verify_phone ? '手机已绑定' : '手机未绑定'"
                             placement="top"
                         >
-                            <a href="/dashboard/#/phone"><i class="el-icon-mobile-phone"></i></a>
+                            <a href="/dashboard/#/phone" :class="info.verify_phone && 'done'"><i class="el-icon-mobile-phone"></i></a>
                         </el-tooltip>
                     </span>
                     <span class="u-group" v-if="group > 60">
@@ -90,7 +90,10 @@
                         </div>
                         <div class="u-credit-op">
                             <router-link class="el-button el-button--primary el-button--mini" to="/points"
-                                >兑换</router-link
+                                >记录</router-link
+                            >
+                            <a class="el-button el-button--primary el-button--mini is-disabled" href="/vip/mall" target="_blank"
+                                >兑换</a
                             >
                         </div>
                     </div>

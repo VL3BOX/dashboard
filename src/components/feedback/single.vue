4816274b101763f6e4f0c98d8f15f5082b4a4c46
litthy <litthy@qq.com>
2022-07-05 00:48:24 +0800

    feat: 反馈单页

--- src/components/feedback/single.vue
+++ src/components/feedback/single.vue
@@ -1,69 +1,45 @@
 <template>
     <div class="m-feed-single" v-if="data">
-        <el-button icon="el-icon-arrow-left" size="small" @click="back">返回列表</el-button>
         <main class="m-single-content" v-loading="loading">
-            <div class="m-single-item">
-                <el-tag effect="dark" size="small" :color="statusColors[data.status]">{{ statusMap[data.status] }}</el-tag>
-                <div class="">
-                    <span class="u-label">分类：</span>
+            <div class="m-type">
+                <span class="u-tag" :style="`border:1px solid ${statusColors[data.status]}`">{{ statusMap[data.status] }}</span>
+                <div class="u-type">
+                    <span class="u-label">BUG分类：</span>
                     <span class="u-value">{{ types[data.type] }}</span>
-                </div>
-                <div class="">
-                    <span class="u-label">类型：</span>
                     <span class="u-value">{{ subtypes[data.subtype] }}</span>
                 </div>
             </div>
-            <div class="m-single-item">
-                <div class="u-user">
-                    <span class="u-label">提交人</span>
-                    <a class="u-item-pic" :href="authorLink(data.user.id)" target="_blank">
-                        <img
-                            class="u-item-avatar"
-                            :src="showAvatar(data.user.avatar)"
-                        />
-                    </a>
-                    <a
-                        class="u-item-name"
-                        :href="authorLink(data.user.id)"
-                        target="_blank"
-                    >{{ data.user.display_name }}</a>
+            <div class="m-user">
+                <span class="u-label">提交者：</span>
+                <a class="u-avatar" :href="authorLink(data.user.id)" target="_blank">
+                    <img :src="showAvatar(data.user.avatar)" />
+                </a>
+                <a class="u-item-name" :href="authorLink(data.user.id)" target="_blank">{{ data.user.display_name }}</a>
+                <span class="u-time">提交时间：{{ formateTime(data.created_at) }}</span>
+            </div>
+            <div class="m-dev">
+                <span class="u-label">分配至</span>
+                <div class="u-item">
+                    <template v-if="data.assign_user">
+                        <a class="u-avatar" :href="authorLink(data.assign_user.id)" target="_blank">
+                            <img :src="showAvatar(data.assign_user.avatar)" />
+                        </a>
+                        <a class="u-item-name" :href="authorLink(data.assign_user.id)" target="_blank">{{ data.assign_user.display_name }}</a>
+                    </template>
+
                 </div>
-                <div class="u-time">
-                    <span class="u-label">时间：</span>
-                    <span class="u-label">{{ formateTime(data.created_at) }}</span>
+                <div class="u-item">
+                    <span class="u-label">仓库</span>
+                    <a class="u-item-name" :href="formateGithub(data.repository)">{{ data.repository }}</a>
                 </div>
             </div>
-            <div class="m-single-item">
-                <div class="u-user" v-if="data.assign_user">
-                    <span class="u-label">分配至</span>
-                    <a class="u-item-pic" :href="authorLink(data.assign_user.id)" target="_blank">
-                        <img
-                            class="u-item-avatar"
-                            :src="showAvatar(data.assign_user.avatar)"
-                        />
-                    </a>
-                    <a
-                        class="u-item-name"
-                        :href="authorLink(data.assign_user.id)"
-                        target="_blank"
-                    >{{ data.assign_user.display_name }}</a>
-                </div>
-                <div class="u-time">
-                    <span class="u-label">仓库：</span>
-                    <a class="u-label" :href="formateGithub(data.repository)">{{ data.repository }}</a>
-                </div>
+            <div class="m-textarea">
+                <el-divider>反馈内容</el-divider>
+                {{ data.content }}
             </div>
-            <div class="m-single-item">
-                <span class="u-label">内容：</span>
-                <span class="u-value">{{ data.content }}</span>
-            </div>
-            <div class="m-single-item" v-if="data.images && data.images.length">
-                <span class="u-label">反馈截图：</span>
-                <el-image
-                    style="width: 100px; height: 100px"
-                    :src="data.images[0]"
-                    lazy
-                    :preview-src-list="data.images">
+            <div class="m-textarea">
+                <el-divider>反馈截图</el-divider>
+                <el-image class="u-img" :src="data.images[0]" lazy :preview-src-list="data.images">
                 </el-image>
             </div>
         </main>
@@ -79,14 +55,14 @@ export default {
     name: "FeedbackSingle",
     data() {
         return {
-            data: '',
+            data: "",
 
             loading: false,
 
             types,
             subtypes,
             statusMap,
-            statusColors
+            statusColors,
         };
     },
     computed: {
@@ -124,44 +100,10 @@ export default {
         },
         formateGithub(val) {
             return `https://github.com/JX3BOX/${val}`;
-        }
+        },
     },
 };
 </script>
-
-<style lang="less" scoped>
-.m-feed-single {
-    .m-single-content {
-        margin-top: 10px;
-
-        .m-single-item {
-            display: flex;
-            gap: 20px;
-            .u-label {
-                color: #999;
-            }
-
-            .u-user, .u-time {
-                display: flex;
-                align-items: center;
-                .u-item-pic {
-                    .dbi;
-                    .x;
-                    .pr;
-                    .auto(x);
-                    .size(68px);
-                    transition: 0.2s ease-in-out;
-                    &:hover{
-                        filter: saturate(120%) brightness(110%);
-                    }
-                }
-                .u-item-avatar {
-                    .r(100%);
-                }
-            }
-
-        }
-
-    }
-}
+<style lang="less" >
+    @import url("~@/assets/css/feedback_single.less");
 </style>

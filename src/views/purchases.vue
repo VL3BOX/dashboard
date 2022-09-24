67b139169d63c809d32a63695accb7293c2c6af8
Kaviilee <jiaweilee95@126.com>
2022-09-25 01:34:09 +0800

    fix: 我的购买列表

--- src/views/purchases.vue
+++ src/views/purchases.vue
@@ -1,34 +1,117 @@
 <template>
     <div class="m-dashboard m-dashboard-work m-dashboard-purchases">
         <h2 class="u-title"><i class="el-icon-shopping-cart-2"></i> 我的购买</h2>
+
+        <el-table class="m-table" :data="list" v-loading="loading" @row-click="rowClick" @filter-change="filterChange" show-header cell-class-name="u-table-cell" header-cell-class-name="u-header-cell">
+            <el-table-column label="购买类型" :filters="postTypeFilters" :filter-multiple="false"  column-key="post_type">
+                <template slot-scope="{ row }">
+                    {{ postType(row.post_type) }}
+                </template>
+            </el-table-column>
+            <el-table-column label="货币类型">
+                <template slot-scope="{ row }">
+                    {{ priceType(row.price_type) }}
+                </template>
+            </el-table-column>
+            <el-table-column label="货币数量" prop="price_count" />
+            <el-table-column label="购买时间" prop="created_at" />
+        </el-table>
+
+        <el-pagination class="m-pagination" background :page-size="limit" :hide-on-single-page="true" :current-page.sync="page" layout="total, prev, pager, next, jumper" :total="total"></el-pagination>
     </div>
 </template>
 
 
 <script>
 import { getPurchases } from "@/service/purchases"
+import { priceType, postType } from "@/assets/data/purchase.json"
+import { getLink } from "@jx3box/jx3box-common/js/utils"
 export default {
     name: 'purchasesList',
     data () {
         return {
+            loading: false,
             list: [],
             page: 1,
+            post_type: '',
             limit: 10,
             total: 0,
         }
     },
-    mounted() {
-        this.loadData()
+    computed: {
+        params() {
+            return {
+                post_type: this.post_type,
+                pageIndex: this.page,
+                pageSize: this.limit,
+            }
+        },
+        postTypeFilters() {
+            return Object.keys(postType).map((key) => {
+                return {
+                    text: postType[key],
+                    value: key,
+                }
+            })
+        },
+    },
+    watch: {
+        params: {
+            deep: true,
+            immediate: true,
+            handler() {
+                this.loadData()
+            },
+        }
     },
     methods: {
         loadData() {
-            getPurchases().then(res => {
+            this.loading = true
+            getPurchases(this.params).then(res => {
                 this.list = res.data.data.list || []
                 this.page = res.data.data.page.index || 1
                 this.limit = res.data.data.page.pageSize || 10
                 this.total = res.data.data.page.total || 0
+            }).finally(() => {
+                this.loading = false
             })
-        }
+        },
+        rowClick(row) {
+            const url = getLink(row.post_type, row.post_id)
+            if (url) {
+                window.open(url, '_blank')
+            }
+        },
+        filterChange(filters) {
+            this.post_type = filters.post_type[0] || ''
+        },
+
+        // filters
+        priceType(type) {
+            return priceType[type]
+        },
+        postType(type) {
+            return postType[type]
+        },
     }
 }
 </script>
+
+<style lang="less">
+.m-dashboard-purchases {
+    .m-table {
+        .u-header-cell {
+            .fz(12px);
+            color: @color;
+        }
+        .u-table-cell {
+            .fz(12px);
+        }
+    }
+    .m-pagination {
+        .x;
+        .pt(20px);
+        overflow-x: auto;
+    }
+}
+</style>

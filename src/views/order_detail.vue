f62c3e147892215d30dcc24c15582fe39d75de5a
litthy <litthy@qq.com>
2022-11-30 16:26:18 +0800

    feat: detail

--- src/views/order_detail.vue
+++ src/views/order_detail.vue
@@ -5,8 +5,37 @@
         </div>
 
         <div class="m-content">
-            <!-- 收货地址 -->
+            <!-- 订单进度条 -->
+            <el-steps :active="2" finish-status="success" align-center>
+                <el-step title="兑换商品" description="2022-12-24 12:22:54"></el-step>
+                <el-step title="积分扣除" description="2022-12-24 12:22:54"></el-step>
+                <el-step title="物品发货" description="2022-12-24 12:22:54"></el-step>
+                <el-step title="确认收货" description=""></el-step>
+                <el-step title="评价" description=""></el-step>
+                <el-step title="订单关闭" description=""></el-step>
+            </el-steps>
+
             <!-- 订单详情 -->
+            <div class="m-order">
+                <div class="m-address">
+                    <div class="u-line u-name">
+                        <span class="u-label">收货地址：</span>
+                        <span class="u-value">
+                            {{ address.actual_contact }} , {{ address.actual_phone }} ,
+                            {{ address.actual_address }}
+                        </span>
+                    </div>
+                    <div class="u-line u-order">
+                        <span class="u-label">订单编号：</span>
+                        <span class="u-value">{{ address.order_no }}</span>
+                    </div>
+                    <div class="u-line u-remark">
+                        <span class="u-label">留言：</span>
+                        <span class="u-value">{{ address.remark || "-" }}</span>
+                    </div>
+                </div>
+                <div class="m-goods"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,12 +46,18 @@ export default {
     name: "record",
     data: function () {
         return {
-            data: [],
+            data: {},
         };
     },
     computed: {
         id() {
             return this.$route.params.id;
+        },
+        address() {
+            return this.data.shipAddress || {};
+        },
+        goods() {
+            return this.data.order;
         },
     },
 

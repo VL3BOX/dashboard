044b1b36e888106598528eb951b338f02aaaca6d
zixuan1986 <litthy@qq.com>
2022-12-04 14:05:32 +0800

    fix: detail

--- src/views/order_detail.vue
+++ src/views/order_detail.vue
@@ -16,7 +16,7 @@
             </el-steps>
 
             <!-- 订单详情 -->
-            <div class="m-order">
+            <div class="m-order" v-if="order">
                 <div class="m-address">
                     <div class="u-line u-name">
                         <span class="u-label">收货地址：</span>
@@ -26,15 +26,39 @@
                         </span>
                     </div>
                     <div class="u-line u-order">
+                        <span class="u-label">下单时间：</span>
+                        <span class="u-value">{{ order.created_at }}</span>
+                    </div>
+                    <div class="u-line u-order">
                         <span class="u-label">订单编号：</span>
-                        <span class="u-value">{{ address.order_no }}</span>
+                        <span class="u-value">{{ order.order_no }}</span>
                     </div>
                     <div class="u-line u-remark">
                         <span class="u-label">留言：</span>
                         <span class="u-value">{{ address.remark || "-" }}</span>
                     </div>
                 </div>
-                <div class="m-goods"></div>
+                <div class="m-goods">
+                    <img class="u-img" :src="goods.goods_images[0]" />
+                    <div class="m-info">
+                        <span class="u-title">{{ goods.title }}</span>
+                        <span>购买数量：{{ order.goods_num }}</span>
+                        <div class="u-consume">
+                            <span>购买消耗：</span>
+                            <div class="u-box">
+                                <span v-if="goods.price_cny">金箔：<b>{{ goods.price_cny * order.goods_num }}</b></span>
+                                <span v-if="goods.price_boxcoin"
+                                    >盒币：<b>{{ goods.price_boxcoin * order.goods_num }}</b></span
+                                >
+                                <span v-if="goods.price_points">积分：<b>{{ goods.price_points * order.goods_num }}</b></span>
+                            </div>
+                        </div>
+                        <span>邮费：{{ goods.postage ? goods.postage / 100 + "元" : "包邮" }}</span>
+                        <span v-if="goods.postage">邮费状态：{{ order.postage_status }}</span>
+                        <span>支付状态：{{ payStatus[order.pay_status] }} </span>
+                        <span>订单状态：{{ orderStatus[order.order_status] }} </span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -42,8 +66,9 @@
 
 <script>
 import { getOrderId } from "@/service/goods";
+import { orderStatus, payStatus } from "../assets/data/mall.json";
 export default {
-    name: "record",
+    name: "orderDetail",
     data: function () {
         return {
             data: {},
@@ -56,8 +81,17 @@ export default {
         address() {
             return this.data.shipAddress || {};
         },
-        goods() {
+        order() {
             return this.data.order;
+        },
+        goods() {
+            return this.data.goods;
+        },
+        payStatus() {
+            return payStatus;
+        },
+        orderStatus() {
+            return orderStatus;
         },
     },
 

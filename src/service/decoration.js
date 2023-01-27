8d008ba51ce48b04d85be6d2d3074523f63993d1
Kaviilee <jiaweilee95@126.com>
2023-01-27 17:51:35 +0800

    feat: emotion

--- src/service/decoration.js
+++ src/service/decoration.js
@@ -1,18 +1,21 @@
 import { $cms } from "@jx3box/jx3box-common/js/https.js";
 import axios from 'axios'
-import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+import { __imgPath, __dataPath } from "@jx3box/jx3box-common/data/jx3box.json";
 function getDecoration(params) {
-    return $cms().get(`/api/cms/user/decoration`,{
+    return $cms().get(`/api/cms/user/decoration`, {
         params
     });
 }
 function setDecoration(data) {
-    return $cms().post(`/api/cms/user/decoration`,data);
+    return $cms().post(`/api/cms/user/decoration`, data);
 }
-function getDecorationJson(){
+function getDecorationJson() {
     let url = __imgPath + "decoration/index.json"
     return axios.get(url)
 }
+function getEmotion(){
+    return axios.get(`${__dataPath}emotion/output/catalog.json`);
+}
 export {
-    getDecoration,setDecoration,getDecorationJson
+    getDecoration, setDecoration, getDecorationJson, getEmotion
 };
--- src/views/theme.vue
+++ src/views/theme.vue
@@ -6,17 +6,13 @@
 
 <script>
 import uc from "@/components/uc.vue";
+import { tabList } from "@/assets/data/theme.json";
 export default {
     name: "theme",
     props: [],
     data: function() {
         return {
-            tabList: [
-                { name: "frame", icon: "el-icon-camera", label: "头像框" },
-                { name: "theme", icon: "el-icon-s-shop", label: "主题风格" },
-                { name: "emotion", icon: "el-icon-picture-outline-round", label: "表情包" },
-                { name: "honor", icon: "el-icon-s-flag", label: "称号" }
-            ],
+            tabList
         };
     },
     computed: {},
